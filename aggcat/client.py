4b9ef28f845d9160f4de20e32ef90ffeb9e34494
Steve Recio <stevenrecio@gmail.com>
2014-08-18 20:09:33 -0400

    let's see what this does...

--- aggcat/client.py
+++ aggcat/client.py
@@ -157,9 +157,8 @@ class AggcatClient(object):
                 self._refresh_client()
                 self._make_request(path, method, body, query, headers)
 
-        if response.status_code not in [200, 201, 401]:
-            import pdb; pdb.set_trace()
-            raise HTTPError('Status Code: %s, Response %s' % (response.status_code, response.text,))
+#        if response.status_code not in [200, 201, 401]:
+#            raise HTTPError('Status Code: %s, Response %s' % (response.status_code, response.text,))
 
         if return_obj:
             try:
